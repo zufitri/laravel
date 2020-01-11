a9d538458bc43e29f9892a712903bd1a043a2ec6
Your Name <amarszepti@gmail.com>
2020-01-11 16:05:17 +0700

    laravel

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        $this->call(BukuSeeder::class);
+        $this->call(PenerbitSeeder::class);
+
+        $this->call(PenjualSeeder::class);
+        $this->call(PembeliSeeder::class);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use App\Pembeli;
+Route::get('/', function () {
+    return Pembeli::all();
+});
+
+Route::get('halo', function () {
+    return 'halo hola';
+});
+//requered paramenter
+Route::get('user/{nama}/{alamat}/{nomor}', function ($nama,$alamat,$nomor) {
+    return 'halo'.$nama.' yang beralamat kan di '.$alamat.' nomor '.$nomor;
+});
+//
+Route::get('angkot/{jurusan}/{warna}', function () {
+    $jurusan ='cibaduyut-karansetra';
+    $data ='angkot jurusan:';
+    $warna='warna angkot adalah: merah';
+    return $data.' '.$jurusan.' <br>'.$warna;
+});
+//
+Route::get('variable', function () {
+    $data1= 'data1';
+    $data2='data2';
+
+    $data3='data3';
+
+    $data4='data4';
+
+    $data5='data5';
+    return "$data1 <br> $data2<br> $data3<br> $data4<br> $data5";
+});
+
+//optional[tidak wajib] paramenter
+Route::get('nilai/{nilai?}', function ($nilai= 'kosong') {
+    return $nilai;
+});
+
+
+
+
+
+
+
+
+Route::get('hasil/{nama}/{nilai?}', function ($nama,$nilai=' tidak diketahui <br>dan tidak mendapatkan grade') {
+if ($nilai>50){
+    echo 'grade A';
+}
+elseif ($nilai<=50 && $nilai >=40){
+    echo 'grade B';
+}
+elseif ($nilai<=40 && $nilai >=30){
+    echo 'grade C';
+}
+elseif ($nilai<=50 && $nilai >=40){
+    echo 'grade D';
+}
+elseif ($nilai<=20 && $nilai >=10 ){
+    echo 'grade E';
+    }
+
+    return '<br>'.'nama '.$nama.'<br>'.'nilai'.$nilai;
+});
